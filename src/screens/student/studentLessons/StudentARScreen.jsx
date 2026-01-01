7d0287b70c2314a08aca921b6fc9a53bc07554d7
hedy-kh <hedykhkh@gmail.com>
2026-01-01 01:35:59 +0100

    make local app build

--- src/screens/student/studentLessons/StudentARScreen.jsx
+++ src/screens/student/studentLessons/StudentARScreen.jsx
@@ -1,52 +1,44 @@
-// import React, { useState, useRef } from "react";
-// import {
-//   ViroARSceneNavigator,
-//   ViroARScene,
-//   ViroText,
-//   Viro3DObject,
-//   ViroDirectionalLight,
-//   ViroImage,
-// } from "@reactvision/react-viro";
+import React from "react";
+import {
+  ViroARSceneNavigator,
+  ViroARScene,
+  ViroText,
+  ViroDirectionalLight,
+  ViroImage,
+} from "@reactvision/react-viro";
 
-// export default function StudentARScreen({ route }) {
-//   const { arView } = route.params;
+const ARScene = (props) => {
+  const { arView } = props.sceneNavigator.viroAppProps;
 
-//   const ARScene = () => {
-//     return (
-//       <ViroARScene>
-//         <ViroDirectionalLight color="#ffffff" direction={[0, -1, 0]} />
-
-//         {/* Example Text */}
-//         <ViroText
-//           text={arView.name}
-//           position={[0, 0, -1]}
-//           style={{ fontSize: 30, color: "white" }}
-//         />
-
-//         {/* Example 3D Model when type === 3d_model */}
-//         {arView.type === "3d_model" && (
-//           <ViroImage
-//             source={require("../../../../assets/images/africamap.jpg")}
-//             position={[0, -0.5, -1]}
-//             scale={[0.5, 0.5, 0.5]}
-//           />
-//         )}
-//       </ViroARScene>
-//     );
-//   };
-
-//   return <ViroARSceneNavigator initialScene={{ scene: ARScene }} autofocus />;
-// }
-
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const StudentARScreen = () => {
   return (
-    <View>
-      <Text>StudentARScreen</Text>
-    </View>
-  )
-}
+    <ViroARScene>
+      <ViroDirectionalLight color="#ffffff" direction={[0, -1, 0]} />
 
-export default StudentARScreen
+      <ViroText
+        text={arView?.name || "No name"}
+        position={[0, 0, -1]}
+        style={{ fontSize: 30, color: "white" }}
+      />
+
+      {arView?.type === "3d_model" && (
+        <ViroImage
+          source={require("../../../../assets/images/africamap.jpg")}
+          position={[0, -0.5, -1]}
+          scale={[0.5, 0.5, 0.5]}
+        />
+      )}
+    </ViroARScene>
+  );
+};
+
+export default function StudentARScreen({ route }) {
+  const { arView } = route.params;
+
+  return (
+    <ViroARSceneNavigator
+      initialScene={{ scene: ARScene }}
+      viroAppProps={{ arView }}
+      autofocus
+    />
+  );
+}
